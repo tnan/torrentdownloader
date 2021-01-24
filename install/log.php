17596dee61152ca8ce34aea1eefaf743db293ee4
tnan <mr.tnan@live.com>
2021-01-24 13:50:39 -0800

    Create log.php

---
+++ install/log.php
@@ -0,0 +1,5 @@
+<?php
+$filename = '/var/www/html/install.log';  //about 500MB
+$output = shell_exec('exec tail -n 20 ' . $filename);  //only print last 50 lines
+echo str_replace(PHP_EOL, '<br />', $output);         //add newlines
+?>
