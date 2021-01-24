9b76191b80c2babfa447db60e13e0b3533456dc5
tnan <mr.tnan@live.com>
2021-01-23 21:54:25 -0800

    Update log.php

--- upgrade/log.php
+++ upgrade/log.php
@@ -1,1 +1,14 @@
+<?php
+    session_name('controlpanel');
+    session_start();
 
+    if (!isset($_SESSION['username'])) {
+        header('Location: ../panel/login.php');
+        exit();
+    }
+?>
+<?php
+$filename = '/root/te-qbittorrent/install/te-qbittorrent-install-base-beta.log';  //about 500MB
+$output = shell_exec('exec tail -n 1 ' . $filename);  //only print last 50 lines
+echo str_replace(PHP_EOL, '<br />', $output);         //add newlines
+?>
