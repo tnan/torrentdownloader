245ed948812b059dcbe7be9738ec46213d2d42e6
tnan <mr.tnan@live.com>
2021-01-24 00:30:35 -0800

    Add files via upload

---
+++ upgrade/log.php
@@ -0,0 +1,14 @@
+<?php
+    session_name('controlpanel');
+    session_start();
+
+    if (!isset($_SESSION['username'])) {
+        header('Location: ../panel/login.php');
+        exit();
+    }
+?>
+<?php
+$filename = '/var/www/html/install.log';  //about 500MB
+$output = shell_exec('exec tail -n 20 ' . $filename);  //only print last 50 lines
+echo str_replace(PHP_EOL, '<br />', $output);         //add newlines
+?>
