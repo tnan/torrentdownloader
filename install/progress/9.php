5990395edd32f09471d55e948e1b542f89fb8ec6
tnan <mr.tnan@live.com>
2021-01-24 16:12:07 -0800

    Update 9.php

--- install/progress/9.php
+++ install/progress/9.php
@@ -47,7 +47,7 @@ Instaltion take about 15~20 minutes. Contact <a href="https://whmcs.the-entertai
 <tr>
 <td>Dependent</td>
 <td style="text-align: center;">Installing...</td>
-</tr>a
+</tr>
 <tr>
 <td>qBittorrent</td>
 <td style="text-align: center;"></td>
